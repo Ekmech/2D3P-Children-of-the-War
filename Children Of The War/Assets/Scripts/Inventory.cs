7d940069874991724d8b566ed5f2c57dc7eb6e25
Ekmech <emir.can2@hotmail.com>
2023-07-14 00:26:21 +0300

    Envanter Sistemi Düzenlendi

--- Children Of The War/Assets/Scripts/Inventory.cs
+++ Children Of The War/Assets/Scripts/Inventory.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Bu sýnýf, envanter sistemini temsil eder.
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // Bu deðiþken, envanterdeki öðelerin sayýsýný tutar.
+    public int ItemCount;
+
+    // Bu deðiþken, envanterdeki öðelerin bir listesini tutar.
+    public List<Item> Items;
+
+    // Bu fonksiyon, envanterdeki bir öðeyi ekler.
+    public void AddItem(Item item)
     {
-        
+        Items.Add(item);
+        ItemCount++;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Bu fonksiyon, envanterdeki bir öðeyi çýkarýr.
+    public void RemoveItem(Item item)
     {
-        
+        Items.Remove(item);
+        ItemCount--;
+    }
+
+    // Bu fonksiyon, envanterdeki bir öðenin sayýsýný döndürür.
+    public int GetItemCount()
+    {
+        return ItemCount;
+    }
+
+    // Bu fonksiyon, envanterdeki bir öðeyi döndürür.
+    public Item GetItem(int index)
+    {
+        return Items[index];
     }
 }
