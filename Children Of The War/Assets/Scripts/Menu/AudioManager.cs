f6de83668c1f40a04027c3c5545224f9a042ec91
Ekmech <emir.can2@hotmail.com>
2023-07-17 16:53:20 +0300

    Scriptlere Düzen getirildi
    Collectable kodları yazıldı
    artık su içiliyor ve kraker yenilebiliyor

---
+++ Children Of The War/Assets/Scripts/Menu/AudioManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    public AudioClip music;
+    private void Start()
+    {
+
+        musicSource.clip = music;
+        musicSource.Play();
+    }
+
+}
